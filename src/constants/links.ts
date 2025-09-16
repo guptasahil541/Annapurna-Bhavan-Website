7086a99a6b0846d76fee654f120d7a9c3214c123
Sahil Gupta <guptasahil541@gmail.com>
2025-09-16 23:48:26 +0530

    Added footer section

---
+++ src/constants/links.ts
@@ -0,0 +1,48 @@
+export const QUICK_LINKS = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "About us",
+    path: "/",
+  },
+  {
+    name: "Reviews",
+    path: "/",
+  },
+  {
+    name: "Book a table",
+    path: "/",
+  },
+  {
+    name: "Contact us",
+    path: "/",
+  },
+];
+
+export const MENU_LINKS = [
+  {
+    name: "Food",
+    path: "/",
+  },
+  {
+    name: "Drinks",
+    path: "/",
+  },
+  {
+    name: "Bar",
+    path: "/",
+  },
+];
+
+export const SITE_LINKS = [
+  {
+    name: "Privacy policy",
+    path: "/",
+  },
+  {
+    name: "Sitemap",
+    path: "/",
+  },
+];
